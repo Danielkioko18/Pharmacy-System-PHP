4577062708a56e646606b2564b12da55d60fdfc9
Daniel <danielkioko1844@gmail.com>
2024-02-17 01:18:13 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PHARMACY</title>
+</head>
+
+<body>
+
+  <body>
+    <style>
+      body {
+        background-size: 100%;
+        background-image: url("images/hmpg.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+
+
+      button {
+        color: lightyellow;
+
+      }
+
+      h1 {
+        color: red;
+        font-size: 100px;
+        font-family: elephant;
+      }
+
+      h2 {
+        font-family: cursive;
+        font-size: 100px;
+        color: blue;
+      }
+
+      p {
+        font-family: sans-serif;
+        font-size: 30px;
+        color: green;
+
+      }
+
+      a {
+        text-decoration: none;
+        font-family: sans-serif;
+        font-size: 20px;
+        color: white;
+        font-weight: bold;
+      }
+
+      button {
+        background-color: green;
+        border-radius: 5px;
+        padding: 7px;
+        border: 1px solid green;
+      }
+    </style>
+    <center>
+
+      <h2><b>Welcome to<b /></h2>
+      <h1><b>MEDICARE PHARMACY<b /></h1>
+      <p><i><b>We treat GOD heals</b></i></p>
+      <button>
+        <a href="">ABOUT US</a>
+      </button>
+      <button>
+        <a href="login.php">LOGIN</a>
+      </button>
+    </center>
+
+  </body>
+
+</html>
